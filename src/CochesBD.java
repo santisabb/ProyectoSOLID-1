ac69d558f48742e93b90830ecabe06e660b70d10
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-07-17 21:04:36 -0300

    se implemento la S de SOLID, hacer mas funcional clase CochesBD

---
+++ src/CochesBD.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class CochesBD {
+    ArrayList<Coche> listaCoches = new ArrayList();
+
+    public void agregarCoche(Coche coche){
+        listaCoches.add(coche);
+    }
+
+    public void mostrarLista(){
+        for (int i = 0; i < listaCoches.size() ; i++){
+            System.out.println("Datos del coches numero " + i + ": " + listaCoches.get(i));
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,38 @@ public class Main {
         //Se trata del principio de responsibalidad unica.
         //Cada clase realizara una unica tarea.
         CocheElectrico coche1 = new CocheElectrico();
+        CochesBD listaCoches1 = new CochesBD();
         CocheNafta coche2 = new CocheNafta();
         CocheHibrido coche3 = new CocheHibrido();
-        coche1.setPrecio(50000);
+
+
+        coche1.precio = 50000;
         coche1.setMarca("Renault");
         coche1.setModelo("Zoe");
         coche1.setCantidadPuertas(3);
-        coche1.setRuedaAuxilio(false);
+        coche1.ruedaAuxilio = true;
         coche1.setAutonomia("580 kilometros");
         coche1.setCapacidadBateria(39.22);
-        if(coche1.isRuedaAuxilio()) {
-            System.out.println("Tiene 2 ruedas de repuesto");
-        } else {
-            System.out.println("No tiene ruedas de auxilio, se recomienda que tenga al menos una");
-        }
+        listaCoches1.agregarCoche(coche1);
+
+        coche2.setPrecio(50000);
+        coche2.setMarca("Renault");
+        coche2.setModelo("Zoe");
+        coche2.setCantidadPuertas(3);
+        coche2.setRuedaAuxilio(false);
+        coche2.setCapacidadTanque(500);
+        coche2.setTipoNafta("Super Infinia");
+        listaCoches1.agregarCoche(coche2);
+
+        coche3.setPrecio(50000);
+        coche3.setMarca("Renault");
+        coche3.setModelo("Zoe");
+        coche3.setCantidadPuertas(3);
+        coche3.setRuedaAuxilio(false);
+        coche3.setDuracionBateria(86.12);
+        coche3.setModoElectrico(true);
+        listaCoches1.agregarCoche(coche3);
+
+        listaCoches1.mostrarLista();
     }
 }
