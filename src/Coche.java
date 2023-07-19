746c9ff056add6517a2af0738696111d7e900803
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-07-19 12:36:38 -0300

    reboot, comienzo de cero aplicando S de SOLID en Coche

--- src/Coche.java
+++ src/Coche.java
@@ -1,49 +1,28 @@
 public class Coche {
     int cantidadPuertas;
-    int precio;
+    double precio;
     String marca;
-    String modelo;
-    boolean ruedaAuxilio;
+
+    public Coche() {
+        this.marca = marca;
+        this.precio = precio;
+        this.cantidadPuertas = cantidadPuertas;
+    }
 
 
     public int getCantidadPuertas() {
         return cantidadPuertas;
     }
 
-    public void setCantidadPuertas(int cantidadPuertas) {
-        this.cantidadPuertas = cantidadPuertas;
-    }
 
-    public int getPrecio(){
+    public double getPrecio(){
         return precio;
     }
 
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    }
 
     public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public boolean isRuedaAuxilio() {
-        return ruedaAuxilio;
-    }
-
-    public void setRuedaAuxilio(boolean ruedaAuxilio) {
-        this.ruedaAuxilio = ruedaAuxilio;
-    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //Aplicar el principio S de S.O.L.I.D.
-        //Se trata del principio de responsibalidad unica.
-        //Cada clase realizara una unica tarea.
-        CocheElectrico coche1 = new CocheElectrico();
-        CochesBD listaCoches1 = new CochesBD();
-        CocheNafta coche2 = new CocheNafta();
-        CocheHibrido coche3 = new CocheHibrido();
 
-
-        coche1.precio = 50000;
-        coche1.setMarca("Renault");
-        coche1.setModelo("Zoe");
-        coche1.setCantidadPuertas(3);
-        coche1.ruedaAuxilio = true;
-        coche1.setAutonomia("580 kilometros");
-        coche1.setCapacidadBateria(39.22);
-        listaCoches1.agregarCoche(coche1);
-
-        coche2.setPrecio(50000);
-        coche2.setMarca("Toyota");
-        coche2.setModelo("Etios");
-        coche2.setCantidadPuertas(3);
-        coche2.setRuedaAuxilio(false);
-        coche2.setCapacidadTanque(500);
-        coche2.setTipoNafta("Super Infinia");
-        listaCoches1.agregarCoche(coche2);
-
-        coche3.setPrecio(50000);
-        coche3.setMarca("Ford");
-        coche3.setModelo("K");
-        coche3.setCantidadPuertas(3);
-        coche3.setRuedaAuxilio(false);
-        coche3.setDuracionBateria(86.12);
-        coche3.setModoElectrico(true);
-        listaCoches1.agregarCoche(coche3);
-
-        listaCoches1.mostrarLista();
     }
 }
