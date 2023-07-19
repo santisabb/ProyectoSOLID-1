524f30a34a3ab63364e52d5aef12053ba7ac78fb
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-07-19 12:08:55 -0300

    se mejoro mostrar lista

--- src/CochesBD.java
+++ src/CochesBD.java
@@ -9,7 +9,16 @@ public class CochesBD {
 
     public void mostrarLista(){
         for (int i = 0; i < listaCoches.size() ; i++){
-            System.out.println("Datos del coches numero " + i + ": " + listaCoches.get(i).marca);
+            System.out.println("Datos del coches numero " + i + ": " + listaCoches.get(i).getMarca()
+                    + ", " + listaCoches.get(i).getModelo()
+                    + ", " + listaCoches.get(i).getPrecio()
+                    + ", " + listaCoches.get(i).getCantidadPuertas());
+
+            if(listaCoches.get(i).isRuedaAuxilio()) {
+                System.out.println("Tiene 2 ruedas de auxilio");
+            } else{
+                System.out.println("No tiene ruedas de repuesto");
+            }
         }
     }
 }
